2675dab85da17405e182b415348bfeb60e856e62
nahidraz23 <nzamanraz@gmail.com>
2024-06-30 21:35:43 +0600

    banner section done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,19 +1,26 @@
 import Link from "next/link";
+import nahidraz from '../../../public/images/nahidrazcover.png'
+import Image from "next/image";
 
 const Banner = () => {
     return (
-        <div className="flex flex-col gap-8">
-            <div>
-                <div className="font-semibold text-3xl">
-                    <p>I am <span className="text-purple-600">Nahiduzzaman Raz</span></p>
-                    <p><span className="text-purple-600">Nahid Raz</span> in short</p>
+        <div className="flex items-center justify-around">
+            <div className="flex flex-col gap-8">
+                <div>
+                    <div className="font-semibold text-3xl">
+                        <p>I am <span className="text-primary-purple">Nahiduzzaman Raz</span></p>
+                        <p><span className="text-primary-purple">Nahid Raz</span> in short</p>
+                    </div>
+                </div>
+                <div>
+                    <p>He crafts responsive websites where technologies meet creativity</p>
+                </div>
+                <div>
+                    <Link href={'/contacts'} className="border-2 px-2 py-1 border-primary-purple"><button>Contact-Me</button></Link>
                 </div>
             </div>
             <div>
-                <p>He crafts responsive websites where technologies meet creativity</p>
-            </div>
-            <div>
-                <Link href={'/contacts'} className="border-2 px-2 py-1 border-purple-600"><button>Contact-Me</button></Link>
+                <Image src={nahidraz} alt="Nahid Raz" className="border-b-2 border-primary-purple"></Image>
             </div>
         </div>
     );
