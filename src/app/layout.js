64d1bcdc5b24ace388887c78360b827bf7ada2bd
nahidraz23 <nzamanraz@gmail.com>
2024-10-25 03:30:15 +0600

    top scrollbar added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Navbar from '@/components/shared/Navbar/Navbar'
 import './globals.css'
 import Footer from '@/components/shared/Footer/Footer'
+import AnimatedCursor from 'react-animated-cursor'
 
 
 export const metadata = {
@@ -9,13 +10,31 @@ export const metadata = {
 }
 
 export default function RootLayout ({ children }) {
+  
   return (
     <html lang='en'>
-      <body className='hide-scrollbar'>
+      <body className='hide-scrollbar ' >
+        <AnimatedCursor
+          innerSize={8}
+          outerSize={35}
+          innerScale={1}
+          outerScale={2}
+          outerAlpha={0}
+          hasBlendMode={true}
+          innerStyle={{
+            backgroundColor: 'white'
+          }}
+          outerStyle={{
+            border: '3px solid white'
+          }}
+          trailingSpeed={10}
+        />
         <div className='container mx-auto'>
           <Navbar></Navbar>
         </div>
-        <div className='min-h-[calc(100vh-270px)] container mx-auto'>{children}</div>
+        <div className='min-h-[calc(100vh-270px)] container mx-auto'>
+          {children}
+        </div>
         <Footer></Footer>
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,15 @@
 'use client'
 import HomePage from '@/components/HomePage/HomePage'
-import AnimatedCursor from 'react-animated-cursor'
+import { motion, useMotionValueEvent, useScroll } from 'framer-motion'
 
 export default function Home () {
+  const { scrollYProgress } = useScroll()
+  
   return (
     <main className='min-h-[calc(100vh-270px)] p-4 md:p-0'>
-      <AnimatedCursor
-        innerSize={8}
-        outerSize={35}
-        innerScale={1}
-        outerScale={2}
-        outerAlpha={0}
-        hasBlendMode={true}
-        innerStyle={{
-          backgroundColor: 'white'
-        }}
-        outerStyle={{
-          border: '3px solid white'
-        }}
-        trailingSpeed={10}
+      <motion.div
+        className='progress-bar'
+        style={{ scaleX: scrollYProgress }}
       />
       <HomePage></HomePage>
     </main>
