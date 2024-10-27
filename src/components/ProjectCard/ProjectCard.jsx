f759e6dc6b1d117b5c998e7f971f95c6636a2fbb
nahidraz23 <nzamanraz@gmail.com>
2024-10-28 00:58:29 +0600

    project card image width updated

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -6,7 +6,7 @@ const ProjectCard = ({ title, src, subTitle, liveLink, gitLink, tech }) => {
     return (
         <div className="relative flex h-[450px] w-96 flex-col items-center justify-center overflow-hidden rounded-lg md:shadow-xl">
             <div>
-                <Image src={src} alt="Project thumbnail" className=" w-full h-52 rounded-xl"></Image>
+                <Image src={src} alt="Project thumbnail" className=" w-80 h-52 rounded-xl"></Image>
             </div>
             <div className="py-4 px-2 mb-2 border-b-2 border-white">
                 <p className="text-white text-2xl font-medium">{title}</p>
