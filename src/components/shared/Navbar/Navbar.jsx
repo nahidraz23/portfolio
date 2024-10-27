a4d999ab438ad6e193be80b2138af281fdd61c44
nahidraz23 <nzamanraz@gmail.com>
2024-10-27 18:39:55 +0600

    navbar updated

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
     ]
 
     return (
-        <div className="navbar md:py-8">
+        <div className="navbar md:py-8 border-b-2 border-white">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
